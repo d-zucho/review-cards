281752b1f02fc023c1534a3656e1c50cf1291d62
D_Zucho <danny.nico.dev@outlook.com>
2022-01-01 02:56:37 -0800

    add reverse conditional class

--- src/components/feedbackItem/FeedbackItem.jsx
+++ src/components/feedbackItem/FeedbackItem.jsx
@@ -3,7 +3,7 @@ import './feedbackItem.css'
 
 const FeedbackItem = ({ item }) => {
   return (
-    <Card reverse={false}>
+    <Card className='card'>
       <div className='num-display'>{item.rating}</div>
       <div className='text-display'>{item.text}</div>
     </Card>
